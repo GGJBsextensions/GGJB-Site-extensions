ecf899c0fc81b83b04f13f187641a97507ac981d
GGJBsextensions <victorstanciu69+1@gmail.com>
2026-04-10 11:52:32 +0300

    Update functions.js

--- JS/functions.js
+++ JS/functions.js
@@ -2,5 +2,5 @@
    window.open("https://www.github.com/GGJBsextensions/GGJb-Site-extensions/", "_blank")
  }
 function Youtube() {
-  window.open("https://www.youtube.com/V1ct0rthem3m3r/", "_blank")
+  window.open("https://www.youtube.com/@V1ct0rthem3m3r/", "_blank")
 }
